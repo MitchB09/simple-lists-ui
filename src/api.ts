8f1c7ad040dcf8d7383ead2ff31be7790bdf4859
Mitch Bilensky <mitch.bilensky@gmail.com>
2021-05-08 23:45:19 -0300

    AWS branch config

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ' https://7htm412c60.execute-api.us-east-1.amazonaws.com/dev',
+  baseURL: ' https://sje4kpvmo5.execute-api.us-east-1.amazonaws.com/dev',
   // baseURL: 'http://192.168.2.16:3001/',
   timeout: 5000,
 });
