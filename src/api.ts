85da6e79f42687edc9849c46e44783e2f984e55b
Mitch Bilensky <mitchbilensky@gmail.com>
2022-03-09 21:37:56 -0400

    reset config

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ' https://sje4kpvmo5.execute-api.us-east-1.amazonaws.com/dev',
-  // baseURL: 'http://192.168.2.16:3001/',
+  baseURL: ' https://7htm412c60.execute-api.us-east-1.amazonaws.com/dev',
   timeout: 5000,
 });
 
